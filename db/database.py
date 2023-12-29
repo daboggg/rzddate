eefd6b99b4ced40e1b4393034ddcc5e528b067b9
daboggg <vzinin@list.ru>
2023-12-29 10:13:26 +0300

    fix data_url

--- db/database.py
+++ db/database.py
@@ -6,7 +6,7 @@ from settings import settings
 
 BASE_DIR = Path(__file__).parent.parent
 
-async_engine = create_async_engine(f"{settings.db_urls.async_db_url}{BASE_DIR}/rzddate.db", echo=True)
+async_engine = create_async_engine(f"{settings.db_urls.async_db_url}///{BASE_DIR}/rzddate.db", echo=True)
 
 
 
